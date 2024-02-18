0cd77159d72410a128a439a9b3f267a510eb32f3
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-02-18 16:32:08 +0000

    Create stub handlers for admin pages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -43,6 +43,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(Auth)
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+
+		mux.Get("/reservations-new", handlers.Repo.AdminNewReservations)
+		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 	})
 
 	return mux
