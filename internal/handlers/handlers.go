2a0135a76da1e3643999b3aebc8602496fdf251d
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-03-06 09:29:57 +0000

    Minor UI fix pass room to reservation page for error handling

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -134,6 +134,13 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	room, err := m.DB.GetRoomByID(roomID)
+	if err != nil {
+		m.App.Session.Put(r.Context(), "error", "invalid data!")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
@@ -142,6 +149,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		StartDate: startDate,
 		EndDate:   endDate,
 		RoomID:    roomID,
+		Room:      room,
 	}
 
 	form := forms.New(r.PostForm)
